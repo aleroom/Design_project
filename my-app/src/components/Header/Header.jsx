686bd20613d2b49943c41e84c67e0c1467b37ac5
mzaharov <mzaharov@softailor.ru>
2024-02-20 22:51:10 +0300

    best refactoring

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import style from "./header.module.css";
 import {useDispatch, useSelector} from "react-redux";
 import {changeLocale} from "../../store/localeSlice/localeSlice";
 import {headerLocales} from "./Header.locale";
+import logo from "../../assets/logo.png"
 
 export default function Header() {
     const dispatch = useDispatch()
@@ -13,7 +14,7 @@ export default function Header() {
             <div className={style.header_wrap}>
                 <div className={style.header_logo}>
                     <img
-                        src="https://s3-alpha-sig.figma.com/img/0b79/1935/ad4c1e296d38a4565fc338e6df855e29?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Q4WktSWhu1bCBsHxGd32raJcuIG8yCxOJig87-oWC0B5TGTWyK0cAEv-BdAB0Nq-bHmRJ8uX2TdkeW2hI4~AfJ4VY1oD626fvlCn7DWMYhCzL~t9k99SieGmMg5gK-~1-wJphn73cXA~2qQkNHdZpBP8biFIlyOFWlBxsakuDz4f8YofG~IVgONf2mf7IuV6lk3XqB2JmzSod5kR9iyoq9QOY9O~Z5d3-tM-lkr5aJbtgQdNJfJ6DTGQo-kU2j03Rv5WcsY~kJVjDYbSFRK-WnWRJyvYnlT4cHWDsmWFvnDw-Q0oh-r0Xc7dTsq8Q-m6gCY7l5ME4a0YR45nhzwCiQ__"
+                        src={logo}
                         alt=""/>
                 </div>
 
