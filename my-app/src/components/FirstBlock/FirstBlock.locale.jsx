33944abe7b71f2d4617720eec741a445b1ebf73b
aleroom <aleroom@yahoo.com>
2024-02-21 17:53:51 +0300

    add logoPage

---
+++ my-app/src/components/FirstBlock/FirstBlock.locale.jsx
@@ -0,0 +1,9 @@
+export const firstBlockLocales = {
+    ru: {
+        text: "Создадим продающий дизайн с wow эффектом  для вашего бизнеса",
+
+    },
+    en: {
+        text: "Create sales design with wow effect for your business",
+    }
+}
--- my-app/src/components/FirstBlock/index.jsx
+++ my-app/src/components/FirstBlock/index.jsx
@@ -1,27 +1,53 @@
 import React from 'react';
 import style from '../FirstBlock/index.module.css';
-import hero from "../../assets/hero.png"
+import hero from "../../assets/hero.png";
+import { firstBlockLocales } from './FirstBlock.locale';
+import {useDispatch, useSelector} from "react-redux";
+
 
 export default function FirstBlock() {
+  const dispatch = useDispatch()
+
+  const locale = useSelector(state => state.locale.locale)
   return (
     <section className={style.fBlock}>
         <div className={style.fBlock_wrap}>
             <div className={style.fBlock_left}>
-                <h1>Создадим <br/> 
-                  продающий дизайн <br/> 
-                  с  wow эффектом для вашего бизнеса</h1>
+                <h1>{firstBlockLocales[locale].text}</h1>
 
                   <div className={style.fBlock_links}>
                     <div className={style.logo_link}>
                        <a href="">логотипы</a>
                     </div>
 
+                    <div className={style.logo_link}>
+                       <a href="">сайты</a>
+                    </div>
+
+                    <div className={style.logo_link}>
+                       <a href="">презентации</a>
+                    </div>
+
                   </div>
+
+                  <div className={style.FB_buttons}>
+                    <button className={style.FB_offer}>
+                      <p>Cпециальное предложение</p> 
+                    </button>
+                    <button className={style.FB_profile}>
+                      <p>Смотреть профиль</p> 
+                    </button>
+                  </div>
+
             </div>
 
             <div className={style.fBlock_right}>
                 <img src={hero} alt="img" />
             </div>
+
+
+            
+            
 
         </div>
 
